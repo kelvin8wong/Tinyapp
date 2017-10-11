e67cf4c140db16f3b94a4542b13aa0d40e6c67e7
kelvin8wong <kelvin8wong@gmail.com>
2017-10-11 22:54:19 +0000

    added body parser

--- express_server.js
+++ express_server.js
@@ -1,9 +1,13 @@
-var express = require("express");
-var app = express();
-var PORT = process.env.PORT || 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 8080; // default port 8080
 app.set("view engine", "ejs")
 
-var urlDatabase = {
+//added body-parser to access POST request
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
@@ -15,6 +19,11 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { shortURL: req.params.id };
+  res.render("urls_show", templateVars);
 });
 
 app.get("/hello", (req, res) => {
